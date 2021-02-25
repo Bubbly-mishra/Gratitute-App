3aeb08f6015a1d669443582bfd9ad3c6660fa587
babli <babimishra002@gmail.com>
2021-02-26 01:21:01 +0530

    'master'

--- app.js
+++ app.js
@@ -12,5 +12,33 @@ todoButton.addEventListener("click", addTodo);
 
 function addTodo(event){
     event.preventDefault();
-    
+   
+    //Todo DIV
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add("todo");
+
+    // Create li
+    const newTodo = document.createElement('li');
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+
+    //check mart button
+
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.append(completedButton);
+
+     //check trash button
+
+     const trashButton = document.createElement('button');
+     trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+     trashButton.classList.add("trash-btn");
+     todoDiv.append(trashButton);
+
+     //append
+     todoList.appendChild(todoDiv);
+     //clear todo input value
+     todoInput.value="";
 }
